7cd4dbf71c21c63f934e2bbebde8eae31514a324
dongchengjie <2424014450@qq.com>
2024-02-29 04:38:06 +0800

    Create test_delay_only.bat

---
+++ tools/test_delay_only.bat
@@ -0,0 +1,20 @@
+@echo off
+rem 参数定义
+set config=https://mirror.ghproxy.com/https://raw.githubusercontent.com/dongchengjie/airport/main/subs/merged/unlimited.yaml
+set output=elected.yaml
+
+set repo=https://github.com/dongchengjie/airport
+set file=subs/merged/elected.yaml
+set token=填写Token
+
+rem 循环执行
+:loop
+rem 节点测试
+meta_speedtest.exe --config %config% -o %output% --timeout 3000 --delay-filter 2000 --log-level debug
+
+rem 推送到Repo
+gfp.exe -r %repo% -f %file% -l ./.meta_speedtest/%output% -t %token%
+
+rem 休眠若干秒
+timeout /t 21600 >nul
+goto loop
